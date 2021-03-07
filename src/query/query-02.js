86c54baf1c18ab9506d019207b20352c64dc60a6
EmilioSanche <72004952+EmilioSanche@users.noreply.github.com>
2021-03-07 11:30:56 +0100

    Update

--- src/query/query-02.js
+++ src/query/query-02.js
@@ -4,19 +4,25 @@
 
 //Buscamos los pacientes que han recibido la transfusion de sangre y contamos cuantas personas de cada tipo lo han recibido.
 db.patient.aggregate([
-    {$match: {
-        patTransfusion: true
-    }}
+    {
+        $match: {
+            patTransfusion: true
+        }
+    }
     ,
-    {$group: {
-        _id: "$patBloodGroup",
-        count: {$sum: 1}
-    }},
-    {$project:{
-        _id: 0,
-        bloodGroup: "$_id",
-        numPatient: "$count"
-    }}
+    {
+        $group: {
+            _id: "$patBloodGroup",
+            count: { $sum: 1 }
+        }
+    },
+    {
+        $project: {
+            _id: 0,
+            bloodGroup: "$_id",
+            numPatient: "$count"
+        }
+    }
 ])
 
 //{ "bloodGroup" : "AB-", "numPatient" : 1 }
@@ -25,38 +31,67 @@ db.patient.aggregate([
 //{ "bloodGroup" : "B-", "numPatient" : 1 }
 
 
+/*----------------------------------------*/
+
+
+//Miramos cuanta cantidad de cada sangre disponemos, si esa cantidad está por debajo de 1.3 añadimos una advertencia como
+//true. Sino, aparecerá como false.
+db.donar.aggregate([
+    {
+        $group: {
+            _id: "$donarBloodGroup",
+            count: { $sum: "$donarTotal" }
+        }
+    },
+    {
+        $project: {
+            _id: 0,
+            typeBlood: "$_id",
+            bloodTotal: { $round: ["$count", 2] },
+            danger: {
+                $cond: { if: { $gte: ["$count", 1.3] }, then: false, else: true }
+            }
+        }
+    }
+])
+
+//{ "typeBlood" : "0-", "bloodTotal" : 1.7, "danger" : false }
+//{ "typeBlood" : "0+", "bloodTotal" : 2.45, "danger" : false }
+//{ "typeBlood" : "A-", "bloodTotal" : 1.4, "danger" : false }
+//{ "typeBlood" : "AB+", "bloodTotal" : 0.4, "danger" : true }
+//{ "typeBlood" : "B+", "bloodTotal" : 0.7, "danger" : true }
+
+
+/*----------------------------------------*/
+
+
 //Buscamos los tipos de sangre donada y las cantidades desde la segunda mitad del año 2020.
 db.donar.aggregate([
-    {$match: 
-        {$expr: {$eq: [{$year: "$donarDate"},2020]}} 
+    {
+        $match:
+            { $expr: { $eq: [{ $year: "$donarDate" }, 2020] } }
     },
-    {$match: 
-        {$expr: {$gt: [{$month: "$donarDate"},5]}} 
+    {
+        $match:
+            { $expr: { $gt: [{ $month: "$donarDate" }, 5] } }
     },
-    {$group:{
-        _id: "$donarBloodGroup",
-        quantityBlood: {$push: {type: "$donarBloodGroup",quantity: {$sum: "$donarTotal"}}} 
-    }},
-    {$project:{
-        _id:0,
-        quantityBlood:1
-    }},
-    {$merge: "donarQuery1"}
+    {
+        $group: {
+            _id: "$donarBloodGroup",
+            quantityBlood: { $push: { type: "$donarBloodGroup", quantity: { $sum: "$donarTotal" } } }
+        }
+    },
+    {
+        $project: {
+            _id: 0,
+            quantityBlood: 1
+        }
+    },
+    { $sort: { "quantityBlood.quantity": -1 } },
+    { $merge: "donarQuery1" }
 ])
+
 
 //{ "quantityBlood" : [ { "type" : "A-", "quantity" : 1 } ] }
-//{ "quantityBlood" : [ { "type" : "AB+", "quantity" : 0.4 } ] }
 //{ "quantityBlood" : [ { "type" : "B+", "quantity" : 0.7 } ] }
-
-
-//
-db.donar.aggregate([
-    {$group:{
-        _id: "$donarBloodGroup",
-        quantityBlood: {$push: {type: "$donarBloodGroup",quantity: {$sum: "$donarTotal"}}} 
-    }},
-    {$project:{
-        _id:0,
-        quantityBlood:1
-    }}
-])
+//{ "quantityBlood" : [ { "type" : "AB+", "quantity" : 0.4 } ] }
--- src/query/query-03.js
+++ src/query/query-03.js
@@ -3,3 +3,57 @@
 //Hacemos busquedas con el lookup para buscar datos especificos entre las bases de datos.
 
 
+db.patient.aggregate([
+    {
+        $match: {
+            patTransfusion: false
+        }
+    }
+    ,
+    {$lookup:{
+        from: "donar",
+        localField: "patBloodGroup",
+        foreignField: "donarBloodGroup",
+        as: "donar"
+    }},
+    {$project:{
+        _id:0,
+        patBloodGroup:1,
+        totalBlood: {$round: [{$sum: ["$donar.donarTotal"]},2]}
+    }}
+])
+
+//
+
+
+
+
+/*----------------------------------------*/
+
+
+
+
+//Buscamos cuantos donantes hay por cada banco de sangre.
+db.donar.aggregate([
+    {$unwind: "$donarBb"},
+    {$lookup:{
+        from: "bank",
+        localField: "donarBb",
+        foreignField: "bbId",
+        as: "BloodBank"
+    }},
+    {$group:{
+        _id: ["$BloodBank.bbId","$BloodBank.bbName"],
+        count: {$sum: 1}
+    }},
+    {$project:{
+        _id:0,
+        bbName: {$last: "$_id"},
+        numDonar: "$count"
+    }}
+])
+
+//{ "bbName" : [ "The Blood Association" ], "numDonar" : 3 }
+//{ "bbName" : [ "Blood Cross Society" ], "numDonar" : 4 }
+//{ "bbName" : [ "Rotary Blood Bank" ], "numDonar" : 4 }
+//{ "bbName" : [ "Athar Blood Bank" ], "numDonar" : 5 }
